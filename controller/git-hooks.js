46c83698941d9f46a37cd8ef96c0381a5e20fe73
jasonhuang <1248394650@qq.com>
2021-12-02 15:19:38 +0800

    补充机器人推送和merget逻辑

--- controller/git-hooks.js
+++ controller/git-hooks.js
@@ -8,17 +8,52 @@ module.exports = {
    * @author jasonhuang
    */
   init(req, res, next) {
-    console.log(req.body)
-    const { 
+    let { 
       object_kind,
-      user: { name }
-     } = req.body
-    if(object_kind === 'merge_request') {
-      let postData = `${name}于合并了代码`
-      console.log('合并了代码')
-      // axios.post(webhook, postData)
-
+      project = {},
+    } = req.body
+    let { source_branch } = object_attributes || {}
+    console.log(req.body)
+    if(!['dev, master, main'].includes(source_branch)) {
+      return res.send({
+        code: 0,
+        message: 'success'
+      })
     }
+    let content = ''
+    if(object_kind === 'push') {
+      let {
+        ref,
+        user_name,
+        commits
+      } = req.body
+      content = `
+        项目「${project.name}」收到一次push提交[汗]\n
+        提交者:「${user_name}」\n
+        分支:「${ref}」\n
+        最新提交信息: "${commits[0] && commits[0].message}"
+      `
+    } else if(object_kind === 'merge_request') {
+      let { 
+        user,
+        object_attributes = {
+          source_branch,
+          source,
+          target
+        },    // 合并相关信息
+        last_commit
+      } = this.req.body
+      content = `
+        「${user.username}」在「${source_branch}」发起了一个MR\n
+        标题: "${last_commit[0] && last_commit[0].message}"\n
+        查看MR详情
+      `
+    }
+    let postData ={
+      content
+    }
+    console.log('合并了代码')
+    axios.post(webhook, postData)
     res.send({
       code: 0,
       messag: 'success'
--- tasks/config.js
+++ tasks/config.js
@@ -2,6 +2,6 @@ module.exports = {
   url: 'https://note.youdao.com/ynoteshare1/index.html?id=f20811452fd279ad4a97f8abba81acbb&type=note',
   webhook: 'https://www.yunzhijia.com/gateway/robot/webhook/send?yzjtype=0&yzjtoken=46ee2001648842b4aacb9687b70dc431',
   git: {
-    webhook: ''
+    webhook: 'https://www.yunzhijia.com/gateway/robot/webhook/send?yzjtype=0&yzjtoken=d953bfa920324225a751045e15cb6bba'
   }
 }
