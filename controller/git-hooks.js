f55d4d4f991c4c3995a4c3b7124fc1c20fa99e13
jasonhuang <1248394650@qq.com>
2021-12-02 16:16:10 +0800

    修复commit提交人逻辑

--- controller/git-hooks.js
+++ controller/git-hooks.js
@@ -14,15 +14,19 @@ module.exports = {
       project = {},
     } = req.body
     let content = ''
+    // "合并分支 'zjian_dev' 到 'dev'\n\n支付页面新增  ISV数据看板\n\n查看合并请求 appmarketplace"
+    let reg = /\s*查看合并请求.*/
     if(object_kind === 'push') {
       let {
         ref = '',
-        user_name,
+        // user_name,    // 注意不是这里， 要看commit里面author信息
         commits
       } = req.body
+      
       if(!(ref.includes('dev') || ref.includes('master')))  return
-      let lastCommitMessage = commits[0] && commits[0].message && commits[0].message.replace(/\s*.*\s*查看合并请求.*/, '')
-      content = `项目「${project.name}」收到一次push提交[汗]\n提交者:「${user_name}」\n分支:「${ref}」\n最新提交信息: "${lastCommitMessage}"`
+      let lastCommitMessage = commits[0] && commits[0].message && commits[0].message.replace(reg, '')
+      let lastCommitUsername = commits[0] && commits[0].author && commits[0].author.name
+      content = `项目「${project.name}」收到一次push提交[汗]\n提交者:「${lastCommitUsername}」\n分支:「${ref}」\n最新提交信息: "${lastCommitMessage}"`
       let postData ={
         content
       }
@@ -39,7 +43,7 @@ module.exports = {
         last_commit
       } = this.req.body
       if(!(['dev, master, main'].includes(source_branch) || ['dev, master, main'].includes(target_branch))) return
-      let lastCommitMessage = last_commit[0] && last_commit[0].message && last_commit[0].message.replace(/\s*.*\s*查看合并请求.*/, '')
+      let lastCommitMessage = last_commit[0] && last_commit[0].message && last_commit[0].message.replace(reg, '')
       content = `「${user.username}」在「${source_branch}」发起了一个MR\n标题: "${lastCommitMessage}"\n查看MR详情`
       let postData ={
         content
